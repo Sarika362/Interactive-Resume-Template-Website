6848b90734b3c8af852978c70c50e72c21fa2dc3
SARIKA M N <sarika.mn97@gmail.com>
2024-09-27 00:32:16 +0530

    Updated directory

--- WebHoodlums/backend/settings.py
+++ WebHoodlums/backend/settings.py
@@ -121,10 +121,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%(c604j93n7d!f1n5&15dpb-s=%&2^%@6ap990gqk2^*^z8nii'
+# SECRET_KEY = 'django-insecure-%(c604j93n7d!f1n5&15dpb-s=%&2^%@6ap990gqk2^*^z8nii'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-%(c604j93n7d!f1n5&15dpb-s=%&2^%@6ap990gqk2^*^z8nii')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -217,12 +218,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-import os
+# import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # This should point to your collected static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'WebHoodlums' / 'static',  # This should point to your app's static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
